176cdd9e2bf8764548fd6d00eab194c60e059f88
pjsmith404 <pjsmith404@gmail.com>
2025-08-04 20:43:30 +1000

    Add a cleanInput function and tests

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func cleanInput(text string) []string {
+	splitText := strings.Fields(text)
+
+	var loweredText []string
+	for _, word := range splitText {
+		loweredText = append(loweredText, strings.ToLower(word))
+	}
+
+	return loweredText
+}
 
 func main() {
 	fmt.Println("Hello, World!")
---
+++ repl_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input: "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+		{
+			input: "  Charmander \t  Bulbasaur \n  PIKACHU   ",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("Length of %v does not match %v", actual, c.expected)
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("Content of %v does not match %v", actual, c.expected)
+			}
+		}
+	}
+}
