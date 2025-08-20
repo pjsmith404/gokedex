4285d9363a78d9f278aad4c3987b622e9bf12f2d
pjsmith404 <pjsmith404@gmail.com>
2025-08-20 19:49:11 +1000

    Add LocationAreaDetail function for pokeapi and use it in commandExplore

--- command_explore.go
+++ command_explore.go
@@ -6,9 +6,20 @@ import (
 
 func commandExplore(conf *config, subCommand string) error {
 	if subCommand == "" {
-		return fmt.Errorf("Provide an area to explore. ie: explore canalave-city-area")
+		return fmt.Errorf("Provide an area to explore. eg: explore canalave-city-area")
 	}
-	fmt.Println("Explore!")
+
+	fmt.Printf("Exploring %v...\n", subCommand)
+
+	locationAreaDetail, err := conf.pokeapiClient.GetLocationAreaDetail(subCommand)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Found Pokemon:")
+	for _, pokemon := range locationAreaDetail.PokemonEncounters {
+		fmt.Println("-", pokemon.Pokemon.Name)
+	}
 
 	return nil
 }
--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -19,6 +19,59 @@ type ResLocationArea struct {
 	} `json:"results"`
 }
 
+type ResLocationAreaDetail struct {
+	EncounterMethodRates []struct {
+		EncounterMethod struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"encounter_method"`
+		VersionDetails []struct {
+			Rate    int `json:"rate"`
+			Version struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+		} `json:"version_details"`
+	} `json:"encounter_method_rates"`
+	GameIndex int `json:"game_index"`
+	ID        int `json:"id"`
+	Location  struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"location"`
+	Name  string `json:"name"`
+	Names []struct {
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		Name string `json:"name"`
+	} `json:"names"`
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"pokemon"`
+		VersionDetails []struct {
+			EncounterDetails []struct {
+				Chance          int   `json:"chance"`
+				ConditionValues *[]any `json:"condition_values"`
+				MaxLevel        int   `json:"max_level"`
+				Method          struct {
+					Name string `json:"name"`
+					URL  string `json:"url"`
+				} `json:"method"`
+				MinLevel int `json:"min_level"`
+			} `json:"encounter_details"`
+			MaxChance int `json:"max_chance"`
+			Version   struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+		} `json:"version_details"`
+	} `json:"pokemon_encounters"`
+}
+
 func (c *Client) GetLocationArea(pageUrl *string) (ResLocationArea, error) {
 	url := baseUrl + "/location-area"
 	if pageUrl != nil {
@@ -59,3 +112,39 @@ func (c *Client) GetLocationArea(pageUrl *string) (ResLocationArea, error) {
 	return locationArea, nil
 }
 
+func (c *Client) GetLocationAreaDetail(id string) (ResLocationAreaDetail, error) {
+	url := baseUrl + "/location-area/" + id
+
+	data, ok := c.cache.Get(url)
+
+	if !ok {
+		res, err := http.Get(url)
+		if err != nil {
+			return ResLocationAreaDetail{}, err
+		}
+		defer res.Body.Close()
+
+		data, err = io.ReadAll(res.Body)
+		if err != nil {
+			return ResLocationAreaDetail{}, err
+		}
+
+		if res.StatusCode > 299 {
+			return ResLocationAreaDetail{}, fmt.Errorf(
+				"Response failed with status code: %v\nBody:\n%v",
+				res.StatusCode,
+				string(data),
+			)
+		}
+
+		c.cache.Add(url, data)
+	}
+
+	var locationAreaDetail ResLocationAreaDetail
+	err := json.Unmarshal(data, &locationAreaDetail)
+	if err != nil {
+		return ResLocationAreaDetail{}, fmt.Errorf("Error unmarshalling data: %w", err)
+	}
+
+	return locationAreaDetail, nil
+}
